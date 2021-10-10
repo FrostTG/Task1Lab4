4cf91e67bf0e1e6da3f840a7b39ef4bf53f46c13
Anton Gusev <FrostTG@gmail.com>
2021-10-10 19:02:51 +0300

    Добавьте файлы проекта.

---
+++ Task1Lab4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int N2 = 0;
+            for (int i = 0; i < N; i++)
+            {
+                N2 += 2 * i + 1;
+                Console.WriteLine("Квадрат числа {0} равен {1}", i + 1, N2); //Почему выводит только 298 значений квадрата?
+            }
+            Console.ReadKey();
+        }
+    }
+}
